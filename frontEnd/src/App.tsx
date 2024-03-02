ae13d50abdb3e5e4d6934aed78ff33ea0756750a
amirh4336 <amirhossienheydarpoor@gmail.com>
2024-03-02 15:31:51 +0330

    feat: auth context

--- frontEnd/src/App.tsx
+++ frontEnd/src/App.tsx
@@ -1,12 +1,23 @@
 import { RouterProvider } from "react-router-dom";
 import "./App.css";
 import router from "./router/Router";
+import { AuthContext } from "./shared/context/auth-context";
+import { useCallback, useState } from "react";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const login = useCallback(() => {
+    setIsLoggedIn(true);
+  }, []);
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+  }, []);
+
   return (
-    <>
+    <AuthContext.Provider value={{ isLoggedIn: isLoggedIn, logout, login }}>
       <RouterProvider router={router} />
-    </>
+    </AuthContext.Provider>
   );
 }
 
