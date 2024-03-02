b16bd38112fab63a570d6e61b65058c47f4d7960
amirh4336 <amirhossienheydarpoor@gmail.com>
2024-03-02 15:37:46 +0330

    update: navBar for login

--- frontEnd/src/shared/components/Navigation/NavLinks/NavLinks.tsx
+++ frontEnd/src/shared/components/Navigation/NavLinks/NavLinks.tsx
@@ -1,21 +1,32 @@
 import { NavLink } from "react-router-dom";
 import "./NavLink.css";
+import { useContext } from "react";
+import { AuthContext } from "../../../context/auth-context";
 
 const NavLinks = () => {
-  return <ul className="nav-links">
-    <li>
-      <NavLink to="/">ALL USERS</NavLink>
-    </li>
-    <li>
-      <NavLink to="/u1/places">MY PLACES</NavLink>
-    </li>
-    <li>
-      <NavLink to="/places/new">ADD PLACE</NavLink>
-    </li>
-    <li>
-      <NavLink to="/auth">AUTHENTICATE</NavLink>
-    </li>
-  </ul>;
+  const { isLoggedIn } = useContext(AuthContext);
+
+  return (
+    <ul className="nav-links">
+      <li>
+        <NavLink to="/">ALL USERS</NavLink>
+      </li>
+      {isLoggedIn ? (
+        <>
+          <li>
+            <NavLink to="/u1/places">MY PLACES</NavLink>
+          </li>
+          <li>
+            <NavLink to="/places/new">ADD PLACE</NavLink>
+          </li>
+        </>
+      ) : (
+        <li>
+          <NavLink to="/auth">AUTHENTICATE</NavLink>
+        </li>
+      )}
+    </ul>
+  );
 };
 
 export default NavLinks;
