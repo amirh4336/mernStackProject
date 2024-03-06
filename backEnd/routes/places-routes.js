4a0b19da68b6b517dc985abfc064a232e6f6f587
amirh4336 <amirhossienheydarpoor@gmail.com>
2024-03-06 14:35:57 +0330

    feat: install backend & router palces

---
+++ backEnd/routes/places-routes.js
@@ -0,0 +1,54 @@
+const express = require("express")
+
+const router = express.Router()
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    imageUrl: "https://cdn.zoomg.ir/2020/11/the-last-of-us-tv-series-1.jpg",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584,
+    },
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Emp State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    imageUrl: "https://cdn.zoomg.ir/2020/11/the-last-of-us-tv-series-1.jpg",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584,
+    },
+    creator: "u2",
+  },
+];
+
+router.get("/:pid" , (req , res , next) => {
+
+  const placeId = req.params.pid
+
+  const place = DUMMY_PLACES.find(p => p.id === placeId)
+
+  if(!place) {
+    return res.status(404).json({message: "Could not find a place for the provided id."})
+  }
+
+  console.log("Get Request in Places")
+  res.json({place}) 
+})
+
+router.get("/user/:uid" , (req  , res , next) => {
+  const userId = req.params.uid
+
+  const place = DUMMY_PLACES.find(p => p.creator === userId)
+
+  res.json({place})
+})
+
+module.exports = router 
