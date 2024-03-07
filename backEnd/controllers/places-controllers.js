3bd8fbc1d2fb6c634a223d27bc4c9da39dd50d53
amirh4336 <amirhossienheydarpoor@gmail.com>
2024-03-07 19:55:16 +0330

    Update: delete

--- backEnd/controllers/places-controllers.js
+++ backEnd/controllers/places-controllers.js
@@ -2,7 +2,7 @@ const { v4: uuidv4 } = require("uuid");
 
 const HttpError = require("../models/http-errors");
 
-const DUMMY_PLACES = [
+let DUMMY_PLACES = [
   {
     id: "p1",
     title: "Empire State Building",
@@ -44,18 +44,18 @@ const getPlaceById = (req, res, next) => {
   res.json({ place });
 };
 
-const getPlaceByUserId = (req, res, next) => {
+const getPlacesByUserId = (req, res, next) => {
   const userId = req.params.uid;
 
-  const place = DUMMY_PLACES.find((p) => p.creator === userId);
+  const places = DUMMY_PLACES.filter((p) => p.creator === userId);
 
-  if (!place) {
+  if (!places || places.length === 0) {
     return next(
       new HttpError("Could not find a place for the provided id.", 404)
     );
   }
 
-  res.json({ place });
+  res.json({ places });
 };
 
 const createPlace = (req, res, next) => {
@@ -93,24 +93,13 @@ const updatePlace = (req, res, next) => {
 
 const deletePlace = (req, res, next) => {
   const placeId = req.params.pid;
+  DUMMY_PLACES = DUMMY_PLACES.filter((p) => p.id !== placeId);
 
-  let place = DUMMY_PLACES.find((p) => p.id === placeId);
-
-  if (!place) {
-    return next(
-      new HttpError("Could not find a place for the provided id.", 404)
-    );
-  }
-
-  const tempArray = DUMMY_PLACES.filter((p) => p.id !== placeId);
-
-  DUMMY_PLACES = [...tempArray];
-
-  res.status(200).json({ message: "place success fully deleted", place });
+  res.status(200).json({ message: "Deleted place." });
 };
 
 exports.getPlaceById = getPlaceById;
-exports.getPlaceByUserId = getPlaceByUserId;
+exports.getPlacesByUserId = getPlacesByUserId;
 exports.createPlace = createPlace;
 exports.updatePlace = updatePlace;
 exports.deletePlace = deletePlace;
