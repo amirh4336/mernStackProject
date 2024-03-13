0c5c06980a822524c29979e5c2a9215495ef703a
amirh4336 <amirhossienheydarpoor@gmail.com>
2024-03-13 16:39:45 +0330

    validtion delete place

--- backEnd/routes/users-routes.js
+++ backEnd/routes/users-routes.js
@@ -9,16 +9,12 @@ router.post(
   "/signup",
   [
     check("name").not().isEmpty(),
-    check("email").isEmail(),
-    check("password").isLength({ min: 4 }),
+    check("email").normalizeEmail().isEmail(),
+    check("password").isLength({ min: 6 }),
   ],
   usersController.signup
 );
 
-router.post(
-  "/login",
-  [check("email").isEmail(), check("password").isLength({ min: 4 })],
-  usersController.login
-);
+router.post("/login", usersController.login);
 
 module.exports = router;
