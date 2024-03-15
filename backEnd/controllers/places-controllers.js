7609a9eee77948e64c1da1941e2f3be8bef852be
amirh4336 <amirhossienheydarpoor@gmail.com>
2024-03-15 15:45:49 +0330

    feat: connect create plcae to db

--- backEnd/controllers/places-controllers.js
+++ backEnd/controllers/places-controllers.js
@@ -1,7 +1,8 @@
 const { v4: uuidv4 } = require("uuid");
 const { validationResult } = require("express-validator");
 const HttpError = require("../models/http-errors");
-const getCoordsForAddress = require("../util/location")
+const getCoordsForAddress = require("../util/location");
+const Place = require("../models/place");
 
 let DUMMY_PLACES = [
   {
@@ -59,7 +60,7 @@ const getPlacesByUserId = (req, res, next) => {
   res.json({ places });
 };
 
-const createPlace = (req, res, next) => {
+const createPlace = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     console.log(errors);
@@ -68,20 +69,26 @@ const createPlace = (req, res, next) => {
 
   const { title, description, address, creator } = req.body;
 
-  const coordinates =   getCoordsForAddress(address)
+  const coordinates = getCoordsForAddress(address);
 
-  const createPlace = {
-    id: uuidv4(),
+  const createdPlace = new Place({
     title,
     description,
-    location: coordinates,
     address,
+    location: coordinates,
+    image:
+      "https://cdn.zoomg.ir/2021/4/the-tomorrow-war-chris-pratt-sit.jpg?w=768",
     creator,
-  };
+  });
 
-  DUMMY_PLACES.push(createPlace);
+  try {
+    await createdPlace.save();
+  } catch (err) {
+    const error = new HttpError("Creating place failed, please try again", 500);
+    return next(error);
+  }
 
-  res.status(201).json({ place: createPlace });
+  res.status(201).json({ place: createdPlace });
 };
 
 const updatePlace = (req, res, next) => {
@@ -107,8 +114,8 @@ const updatePlace = (req, res, next) => {
 
 const deletePlace = (req, res, next) => {
   const placeId = req.params.pid;
-  if (!DUMMY_PLACES.find(p=> p.id === placeId)) {
-    throw new HttpError("Could not find a place for that id.", 404)
+  if (!DUMMY_PLACES.find((p) => p.id === placeId)) {
+    throw new HttpError("Could not find a place for that id.", 404);
   }
 
   DUMMY_PLACES = DUMMY_PLACES.filter((p) => p.id !== placeId);
