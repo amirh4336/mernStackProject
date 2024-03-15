30265614cff4a9cf0cf2025f3db5238b0562db6c
amirh4336 <amirhossienheydarpoor@gmail.com>
2024-03-15 15:19:01 +0330

    connect to mongodb

--- backEnd/app.js
+++ backEnd/app.js
@@ -1,27 +1,38 @@
-const express = require('express')
-const HttpError = require("./models/http-errors")
-const bodyParser = require('body-parser')
+const express = require("express");
+const HttpError = require("./models/http-errors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-const placesRoutes = require('./routes/places-routes')
-const usersRoutes = require('./routes/users-routes')
+const placesRoutes = require("./routes/places-routes");
+const usersRoutes = require("./routes/users-routes");
 
-const app = express()
+const app = express();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.use( "/api/places" , placesRoutes)
-app.use( "/api/users" , usersRoutes)
+app.use("/api/places", placesRoutes);
+app.use("/api/users", usersRoutes);
 
-app.use((req , res , next ) => {
-  const error = new HttpError("Could not find this route." , 404)
-  throw error
-})
+app.use((req, res, next) => {
+  const error = new HttpError("Could not find this route.", 404);
+  throw error;
+});
 
-app.use((error , req , res , next ) => {
-  if(res.headerSent) {
-    return next(error)
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
   }
-  res.status(error.code || 5000).json({message: error.message || "An unknown error occured!"})
-})
+  res
+    .status(error.code || 5000)
+    .json({ message: error.message || "An unknown error occured!" });
+});
 
-app.listen(5000)
+mongoose
+  .connect("mongodb://root:0gWQjcVpoDD4Z4No0mCIMhrz@denali.liara.cloud:34221/my-app?authSource=admin")
+  .then(() => {
+    app.listen(5000);
+    console.log("server started on port 5000");
+  })
+  .catch((err) => {
+    console.log("we have module error :", err)
+  });
--- backEnd/util/location.js
+++ backEnd/util/location.js
@@ -2,7 +2,7 @@ function getCoordsForAddress(address) {
   const lat1 = Math.random();
   const lng1 = Math.random();
 
-  return { lat: lat1, lng: lng1 };
+  return { lat: lat1*100, lng: lng1*100 };
 }
 
 module.exports = getCoordsForAddress;
