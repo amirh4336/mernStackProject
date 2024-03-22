4c789295c311156491773de54e247d6d535b26c6
amirh4336 <amirhossienheydarpoor@gmail.com>
2024-03-22 22:07:17 +0330

    feat: getting data palces

--- frontEnd/src/places/pages/UpdatePlace/UpdatePlace.tsx
+++ frontEnd/src/places/pages/UpdatePlace/UpdatePlace.tsx
@@ -1,6 +1,5 @@
 import { useParams } from "react-router-dom";
 import "./UpdatePlace.css";
-import { DUMMY_PLACES } from "../UserPlaces";
 import Input from "../../../shared/components/FormElements/Input/Input";
 import { EditInputs, InputName } from "../../../shared/hooks/form-hooks/types";
 import {
@@ -11,8 +10,14 @@ import Button from "../../../shared/components/FormElements/Button/Button";
 import { useForm } from "../../../shared/hooks/form-hooks/form-hooks";
 import { FormEvent, useEffect, useState } from "react";
 import Card from "../../../shared/components/UIElements/Card/Card";
+import { useHttpClient } from "../../../shared/hooks/http-hook";
+// import ErrorModal from "../../../shared/components/UIElements/ErrorModal/ErrorModal";
 const UpdatePlace = () => {
-  const [isLoading, setIsLoading] = useState(true);
+  const { isLoading, sendRequest } = useHttpClient();
+  const [identifedPlace, setIdentifedPlace] = useState<{
+    title: string;
+    description: string;
+  }>();
   const placeId = useParams().placeId;
 
   const [{ inputs, isValid }, inputHandler, setFormData] = useForm<EditInputs>(
@@ -29,26 +34,35 @@ const UpdatePlace = () => {
     false
   );
 
-  const identifedPlace = DUMMY_PLACES.find((p) => p.id === placeId);
+  // const identifedPlace = DUMMY_PLACES.find((p) => p.id === placeId);
 
   useEffect(() => {
-    if (identifedPlace) {
-      setFormData(
-        {
-          title: {
-            value: identifedPlace?.title || "",
-            isValid: true,
+    const sendHandler = async () => {
+      try {
+        const resData = await sendRequest({
+          url: `http://localhost:5000/api/places/${placeId}`,
+        });
+        console.log(resData);
+        setIdentifedPlace(resData.place);
+        setFormData(
+          {
+            title: {
+              value: resData.place?.title || "",
+              isValid: true,
+            },
+            description: {
+              value: resData.place?.description || "",
+              isValid: true,
+            },
           },
-          description: {
-            value: identifedPlace?.description || "",
-            isValid: true,
-          },
-        },
-        true
-      );
-    }
-    setIsLoading(false);
-  }, [setFormData, identifedPlace]);
+          true
+        );
+      } catch (err: any) {
+        console.log(err);
+      }
+    };
+    sendHandler();
+  }, [sendRequest, placeId, setFormData]);
 
   const updatePlaceSubmitHandler = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -74,35 +88,38 @@ const UpdatePlace = () => {
   }
 
   return (
-    !isLoading && (
-      <form className="place-form" onSubmit={updatePlaceSubmitHandler}>
-        <Input
-          id={InputName.title}
-          elementProps="input"
-          type="text"
-          label="Title"
-          validators={[VALIDATOR_REQUIRE()]}
-          errorText="Please enter a valid title."
-          onInput={inputHandler}
-          initialValue={inputs.title.value}
-          initialValid={inputs.title.isValid}
-        />
-        <Input
-          id={InputName.description}
-          elementProps="input"
-          type="text"
-          label="Description"
-          validators={[VALIDATOR_MINLENGTH(5)]}
-          errorText="Please enter a valid description (at least 5 characters)."
-          onInput={inputHandler}
-          initialValue={inputs.description.value}
-          initialValid={inputs.description.isValid}
-        />
-        <Button type="submit" disabled={!isValid}>
-          UPDATE PLACE
-        </Button>
-      </form>
-    )
+    <>
+      {/* {error && <ErrorModal error={error} onClear={clearError} />} */}
+      {!isLoading && (
+        <form className="place-form" onSubmit={updatePlaceSubmitHandler}>
+          <Input
+            id={InputName.title}
+            elementProps="input"
+            type="text"
+            label="Title"
+            validators={[VALIDATOR_REQUIRE()]}
+            errorText="Please enter a valid title."
+            onInput={inputHandler}
+            initialValue={inputs.title.value}
+            initialValid={inputs.title.isValid}
+          />
+          <Input
+            id={InputName.description}
+            elementProps="input"
+            type="text"
+            label="Description"
+            validators={[VALIDATOR_MINLENGTH(5)]}
+            errorText="Please enter a valid description (at least 5 characters)."
+            onInput={inputHandler}
+            initialValue={inputs.description.value}
+            initialValid={inputs.description.isValid}
+          />
+          <Button type="submit" disabled={!isValid}>
+            UPDATE PLACE
+          </Button>
+        </form>
+      )}
+    </>
   );
 };
 
--- frontEnd/src/places/pages/UserPlaces.tsx
+++ frontEnd/src/places/pages/UserPlaces.tsx
@@ -1,37 +1,41 @@
 import { useParams } from "react-router-dom";
 import PlaceList from "../components/PlaceList/PlaceList";
-
-export const DUMMY_PLACES = [
-  {
-    id: "p1",
-    title: "Empire State Building",
-    description: "One of the most famous sky scrapers in the world!",
-    imageUrl: "https://cdn.zoomg.ir/2020/11/the-last-of-us-tv-series-1.jpg",
-    address: "20 W 34th St, New York, NY 10001",
-    location: {
-      lat: 40.7484405,
-      lng: -73.9878584,
-    },
-    creator: "u1",
-  },
-  {
-    id: "p2",
-    title: "Emp State Building",
-    description: "One of the most famous sky scrapers in the world!",
-    imageUrl: "https://cdn.zoomg.ir/2020/11/the-last-of-us-tv-series-1.jpg",
-    address: "20 W 34th St, New York, NY 10001",
-    location: {
-      lat: 40.7484405,
-      lng: -73.9878584,
-    },
-    creator: "u2",
-  },
-];
+import { useEffect, useState } from "react";
+import { useHttpClient } from "../../shared/hooks/http-hook";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner/LoadingSpinner";
 
 const UserPlaces = () => {
   const userId = useParams().userId;
-  const loadedPlaces = DUMMY_PLACES.filter((palce) => palce.creator === userId);
-  return <PlaceList items={loadedPlaces} />;
+
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+  const [loadedPlaces, setLoadedPlaces] = useState([]);
+
+  useEffect(() => {
+    const sendHandler = async () => {
+      try {
+        const resData = await sendRequest({
+          url: `http://localhost:5000/api/places/user/${userId}`,
+        });
+
+        setLoadedPlaces(resData.places);
+      } catch (err: any) {
+        console.log(err);
+      }
+    };
+    sendHandler();
+  }, [sendRequest, userId]);
+  return (
+    <>
+      {error && <ErrorModal error={error} onClear={clearError} />}
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner />
+        </div>
+      )}
+      {!isLoading && loadedPlaces && <PlaceList items={loadedPlaces} />}
+    </>
+  );
 };
 
 export default UserPlaces;
